4db6675125dd9244f7bee56acd15e33785d7b18e
Andrew Hamon <andrew@hamon.cc>
2016-01-24 15:31:39 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+
+
+gem 'rails', '4.2.5'
+
+gem 'rails-api'
+
+gem 'spring', :group => :development
+
+
+gem 'sqlite3'
+
+gem 'active_model_serializers', github: 'rails-api/active_model_serializers'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+TestApp::Application.config.secret_token = 'e7f677a923c998e57285a487b12b0f352065562293423ca39605aa367a5d130863743ab0950d5208b4789072c145c653a47c181b7f22cd41eda22db9c9bfbc35'
